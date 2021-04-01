42a0cc7bebfa5ae317765eca39a3d9c593024cd5
Cory Grewohl <corywalter.grewohl@calbaptist.edu>
2021-03-31 21:23:35 -0700

    hi

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>You Need Music</title>
+</head>
+<body>
+    <div>
+        <a href="genres.php" class="indexButton">Genres</a>
+    </div>
+    <div class = "logoMiddle">
+        <h1>You Need Music</h1>
+        <h3>test</h3>
+        <?php
+        echo "php test2";
+        ?>
+        <form class = "searchBar">
+            <input type = "text" placeholder="Search music catalog">
+            <button type = "submit"></button>
+        </form>
+    </div>
+</body>
+</html>
